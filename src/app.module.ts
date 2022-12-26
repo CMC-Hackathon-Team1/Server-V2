2945fd737d85df57b6906b2eaa78c0b3a402ad02
iamjooon2 <iamjooon2@gmail.com>
2022-12-26 23:11:14 +0900

    initial setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: String(process.env.DEVELOPMENT_DB_HOST),
+      port: Number(process.env.DEVELOPMENT_DB_PORT),
+      username: String(process.env.DEVELOPMENT_DB_USER),
+      password: String(process.env.DEVELOPMENT_DB_PASS),
+      database: 'dev',
+      entities: [],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],

814b5d7d7e783a70649c645ac2555ac141c1ac05
변재호 <bjho606@naver.com>
2023-01-12 22:20:27 +0900

    [fix] client 회원가입 validation pipe 적용, 상태코드 번호 대역 변경

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards, UsePipes, ValidationPipe} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserDTO } from './dto/user.dto';
 import { JWTAuthGuard } from './security/auth.guard.jwt';
@@ -9,6 +9,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
+  @UsePipes(ValidationPipe)
   async registerAccount(@Body() userDTO: UserDTO): Promise<any> {
     return await this.authService.registerUser(userDTO);
   }
--- src/config/baseResponseStatus.ts
+++ src/config/baseResponseStatus.ts
@@ -1,19 +1,18 @@
 const baseResponse = {
-  // Pipe 관련 statusCode
-  // IsNotEmpty: 400
-
   // SUCCESS
   SUCCESS: { statusCode: 100, message: 'SUCCESS' },
 
+  // 서버, Pipe 등 관련 statusCode -> 400~500번대
+
   // DB ERROR
-  DB_ERROR: { statusCode: 200, message: 'DB_ERROR' },
+  DB_ERROR: { statusCode: 500, message: 'DB_ERROR' },
 
   // 회원, 계정 관련
-  USER_ALREADY_EXISTS: { statusCode: 300, message: '이미 가입된 회원입니다.' },
-  USER_NOT_FOUND: { statusCode: 301, message: '회원 정보가 없습니다. 이메일과 비밀번호를 올바르게 입력했는지 확인하세요.' },
+  USER_ALREADY_EXISTS: { statusCode: 1000, message: '이미 가입된 회원입니다.' },
+  USER_NOT_FOUND: { statusCode: 1001, message: '회원 정보가 없습니다. 이메일과 비밀번호를 올바르게 입력했는지 확인하세요.' },
 
   // 프로필 관련
-  PROFILE_COUNT_OVER: { statusCode: 400, message: '사용자 프로필은 3개까지 생성 가능합니다.' },
+  PROFILE_COUNT_OVER: { statusCode: 1050, message: '사용자 프로필은 3개까지 생성 가능합니다.' },
 };
 
 export default baseResponse;

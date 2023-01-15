a2f84e78927abcf5eb211ff09bcf2667a39f75e6
HBSPS <wiyeong79@naver.com>
2023-01-15 16:16:39 +0900

    fix: 상태메시지 null 값 허용
    request body에 상태 메시지가 없는 경우, 빈 문자열을 기본적으로 입력

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -8,8 +8,8 @@ export class ProfilesController {
   constructor(private profilesService: ProfilesService) {}
 
   // API No. 1.1 프로필 생성
-  @UseGuards(JWTAuthGuard)
-  @Post()
+  // @UseGuards(JWTAuthGuard)
+  @Post('create')
   @UsePipes(ValidationPipe)
   createProfile(@Body() createProfileDto: CreateProfileDto) {
     return this.profilesService.createProfile(createProfileDto);

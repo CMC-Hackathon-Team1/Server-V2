a8e9ca3e1f9a12572018c82643a1a6bb2fb9412a
HBSPS <wiyeong79@naver.com>
2023-03-08 13:20:53 +0900

    fix: FCM 토큰 수신 API Body 수정
    userId를 JWT에서 받아오도록 수정
    TODO: 토큰 업데이트 결과 Validation

--- src/alarms/dto/alarmToken.dto.ts
+++ src/alarms/dto/alarmToken.dto.ts
@@ -3,12 +3,6 @@ import { IsNotEmpty } from "class-validator";
 
 export class AlarmTokenDto {
   @ApiProperty({
-    description: '사용자 ID'
-  })
-  @IsNotEmpty()
-  userId: number;
-
-  @ApiProperty({
     description: '푸시 알림 전송용 기기별 토큰값'
   })
   @IsNotEmpty()
